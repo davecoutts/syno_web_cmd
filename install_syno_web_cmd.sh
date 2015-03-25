07ad556d21ffcc77fa83561c0b4eb264a19e50c2
Dave Coutts <dacoutts@gmail.com>
2015-03-25 23:06:28 +0000

    Update install_syno_web_cmd.sh

--- install_syno_web_cmd.sh
+++ install_syno_web_cmd.sh
@@ -7,11 +7,14 @@ mkdir /usr/local/syno_web_cmd
 
 cd /usr/local/syno_web_cmd
 
+# Download bottle. http://bottlepy.org/docs/stable/index.html
 wget http://bottlepy.org/bottle.py
+# Download sh. http://amoffat.github.io/sh
 wget https://raw.githubusercontent.com/amoffat/sh/master/sh.py
 
+# Download bottle web app
 wget https://raw.githubusercontent.com/davecoutts/syno_web_cmd/master/syno_web_cmd.py
+# Download upstart script
 wget https://raw.githubusercontent.com/davecoutts/syno_web_cmd/master/syno_web_cmd.conf
 
 ln -s /usr/local/syno_web_cmd/syno_web_cmd.conf /etc/init/syno_web_cmd.conf
-
