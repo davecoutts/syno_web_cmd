cb2b98a12faed6b40101dcb95d0279059bac4833
Dave Coutts <dacoutts@gmail.com>
2015-03-25 22:17:11 +0000

    Create syno_web_cmd.py

---
+++ syno_web_cmd.py
@@ -0,0 +1,16 @@
+from bottle import route, run
+from sh import syno_poweroff_feasible_check
+
+@route('/')
+def home():
+    return "syno_web_cmd is running\n\nPower off the Synology with 'wget http://SYNOLOGY_IP_ADDRESS:8080/poweroff'"
+
+@route('/poweroff')
+def _poweroff():
+    try:
+        syno_poweroff_feasible_check()
+    except:
+        pass
+    return
+
+run(host='0.0.0.0', port=8080, quiet=True)
