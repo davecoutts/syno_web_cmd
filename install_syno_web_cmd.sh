84d2e1c8004bdeaf2a1de4c55ad233acd345198e
Dave Coutts <dacoutts@gmail.com>
2015-03-25 22:40:45 +0000

    Create install_syno_web_cmd.sh

---
+++ install_syno_web_cmd.sh
@@ -0,0 +1,13 @@
+# rm -rf /usr/local/syno_web_cmd
+mkdir /usr/local/syno_web_cmd
+
+cd /usr/local/syno_web_cmd
+
+wget http://bottlepy.org/bottle.py
+wget https://raw.githubusercontent.com/amoffat/sh/master/sh.py
+
+wget https://raw.githubusercontent.com/davecoutts/syno_web_cmd/master/syno_web_cmd.py
+wget https://raw.githubusercontent.com/davecoutts/syno_web_cmd/master/syno_web_cmd.conf
+
+ln -s /usr/local/syno_web_cmd/syno_web_cmd.conf /etc/init/syno_web_cmd.conf
+
